60d4e21e2d269cf7b40072528a8ae47f9e388b7a
Simon Erhardt <hello@rootlogin.ch>
2018-08-13 21:59:37 +0200

    removed frontend

--- frontend/src/store/plugins.js
+++
@@ -1,9 +0,0 @@
-import {STORAGE_KEY} from './mutations';
-
-const localStoragePlugin = store => {
-    store.subscribe((mutation, {user}) => {
-        window.localStorage.setItem(STORAGE_KEY, JSON.stringify(user));
-    });
-};
-
-export default [localStoragePlugin];
