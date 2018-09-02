d4736590a0aa7e6accc827f947ba5e034025d892
Simon Erhardt <hello@rootlogin.ch>
2018-09-02 22:17:21 +0200

    added first test

--- src/filemanager/handler_test.go
+++ src/filemanager/handler_test.go
@@ -9,9 +9,10 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"github.com/chrootlogin/go-wiki/src/lib/common"
 )
 
-func TestGetConfigHandler(t *testing.T) {
+func TestListFolderHandler(t *testing.T) {
 	assert := assert.New(t)
 
 	w := httptest.NewRecorder()
@@ -30,4 +31,33 @@ func TestGetConfigHandler(t *testing.T) {
 			assert.True(len(resp.Files) > 0)
 		}
 	}
+}
+
+func TestPostFileHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	w := httptest.NewRecorder()
+
+	r := gin.Default()
+	r.POST("/api/raw/*path", loginPostFileHandler)
+
+	req, _ := http.NewRequest("POST", "/api/raw/", nil)
+	r.ServeHTTP(w, req)
+
+	body, err := ioutil.ReadAll(w.Body)
+	if assert.NoError(err) {
+		var resp common.ApiResponse
+		err = json.Unmarshal(body, &resp)
+		if assert.NoError(err) {
+			assert.Equal(http.StatusInternalServerError, w.Code)
+		}
+	}
+}
+
+func loginPostFileHandler(c *gin.Context) {
+	c.Set("user", common.User{
+		Username: "testuser",
+	})
+
+	PostFileHandler(c)
 }
