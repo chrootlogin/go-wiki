89caa9d99d53ad766c0d7098713da36e1112e50b
Simon Erhardt <hello@rootlogin.ch>
2018-09-02 20:40:05 +0200

    second auth check

--- src/auth/auth_test.go
+++ src/auth/auth_test.go
@@ -46,3 +46,30 @@ func TestAuthMiddleware_LoginHandler(t *testing.T) {
 		assert.Equal(http.StatusOK, w.Code)
 	}
 }
+
+func TestAuthMiddleware_LoginHandler2(t *testing.T) {
+	assert := assert.New(t)
+
+	apiReq := ApiLogin{
+		Username: "admin",
+		Password: "admin12345",
+	}
+
+	data, err := json.Marshal(apiReq)
+	if assert.NoError(err) {
+		w := httptest.NewRecorder()
+
+		os.Setenv("SESSION_KEY", "not-a-secret-key")
+		am := GetAuthMiddleware()
+
+		r := gin.Default()
+		r.POST("/user/login", am.LoginHandler)
+
+		req, _ := http.NewRequest("POST", "/user/login", bytes.NewReader(data))
+		req.Header.Add("Content-Type", "application/json")
+		req.Header.Add("Content-Length", string(len(data)))
+		r.ServeHTTP(w, req)
+
+		assert.Equal(http.StatusUnauthorized, w.Code)
+	}
+}
